3870952d49ff0ad21732b8e169961ba3379c4d03
Mateusz Poczta <mapoc23@student.sdu.dk>
2023-11-15 12:40:04 +0100

    re-init project

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "SDU Share Expense"
+include(":app")
+ 
